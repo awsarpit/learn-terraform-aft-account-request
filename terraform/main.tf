88649d537d6f1272a6cfbd25e102e014bc187460
awsarpit <122075856+awsarpit@users.noreply.github.com>
2024-01-12 11:30:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-module "sandbox" {
+module "member" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
@@ -20,8 +20,34 @@ module "sandbox" {
   }
 
   custom_fields = {
-    group = "sandbox"
+    group = "member"
+  }
+}
+
+module "master" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "arpchoud+ct_master@amazon.com"
+    AccountName               = "ArpchoudCTMaster"
+    ManagedOrganizationalUnit = "Root"
+    SSOUserEmail              = "arpchoud+ct_master@amazon.com"
+    SSOUserFirstName          = "AWS Control Tower"
+    SSOUserLastName           = "Admin"
   }
 
-  account_customizations_name = "sandbox"
+  account_tags = {
+    "ManagedBy" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Arpit"
+    change_reason       = "Enroll Existing"
+  }
+
+  custom_fields = {
+    group = "master"
+  }
+
+  account_customizations_name = "master"
 }
